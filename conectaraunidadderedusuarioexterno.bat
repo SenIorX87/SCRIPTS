bfaff078fe82074eea93d874ec5de0b418960dd0
Tropidopy <131480776+SenIorX87@users.noreply.github.com>
2025-09-02 14:29:01 +0200

    Update conectaraunidadderedusuarioexterno.bat
    Ocultar datos privados

--- conectaraunidadderedusuarioexterno.bat
+++ conectaraunidadderedusuarioexterno.bat
@@ -10,9 +10,9 @@ net use V: /delete
 REM net use Z: /delete
 
 
-net use y: /user A3TV_MAD\jmolinagonzaold@atresmediacine.com "\\tifon\dptos$\Noticias y Entretenimiento\Area de Programas y Documentacion\Cine Español\" set /p PASSWORD="Contraseña: "
+net use y: /user A3TV_MAD\ejemplo@correo.com "\\tifon\dptos$\" set /p PASSWORD="Contraseña: "
 
-net use z: /user A3TV_MAD\jmolinagonzaold@atresmediacine.com "\\tifon\aptos$\Comunes\397- Financiero_A3films\" set /p PASSWORD="Contraseña: "
+net use z: /user A3TV_MAD\ejemplo@correo.com "\\tifon\aptos$\" set /p PASSWORD="Contraseña: "
 
 ENDLOCAL
 pause
